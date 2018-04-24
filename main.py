65e90d3e39e67957caad96ca735d50fdae4607f1
Aswin Raj Kharel <ark@Aswins-MacBook-Pro.local>
2018-04-24 16:40:27 -0500

    multiply integrated

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import multiply
 if __name__ == "__main__":
 	# All your test code here
-	
+	v1 = [1, 2, 3]
+	v2 = [8, 9, 10]	
 	# Add the vectors
-	
+	print(multiply.multiply(v1, v2))
 	# Multiply the vectors
